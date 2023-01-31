e0dee89fe731c4cbe087381dc6e3f7ab3294e939
Matee <matiullahsafibbc@gmail.com>
2023-01-31 14:26:38 -0600

    Add cross button

---
+++ script.js
@@ -0,0 +1,9 @@
+// function changeIcon(){
+
+//   let icon = document.getElementById('hamburger');
+//   if(icon.src.match('./assets/hamburger-icon.svg')){
+//       icon.src = './assets/Enabled.png';
+//   }else{
+//       icon.src = './assets/hamburger-icon.svg';
+//   }
+// }
