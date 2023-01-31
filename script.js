12e71883fc8db4baf86316504fc004c83b5011e0
Gardimy Charles <gardimycharles889@gmail.com>
2023-01-31 16:07:25 -0500

    Update style.css and also Script.js

--- script.js
+++ script.js
@@ -7,3 +7,12 @@
 //       icon.src = './assets/hamburger-icon.svg';
 //   }
 // }
+function myfunction() {
+ var nav = document.getElementById('nav');
+ 
+ if (nav.style.display === "grid"){
+	nav.style.display = "none"
+ } else{
+	nav.style.display = "grid"
+ }
+}
