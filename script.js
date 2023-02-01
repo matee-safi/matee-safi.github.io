83ed0db261130ccd69ae80914b69202db16989ca
Matee <matiullahsafibbc@gmail.com>
2023-02-01 06:04:15 -0600

    Update script.js file

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const btn = document.querySelector('#hamburger');
 const nav = document.querySelector('.nav nav');
 const openNav = document.querySelector('.menuOn');
 const closeBtn = document.querySelector('.crossIcon');
+const menuLink = document.querySelectorAll('#menu-link li a');
 
 const toggleMenuDisplay = () => {
   if (nav.style.display !== 'none') {
@@ -15,3 +16,6 @@ const toggleMenuDisplay = () => {
 
 btn.addEventListener('click', toggleMenuDisplay);
 closeBtn.addEventListener('click', toggleMenuDisplay);
+menuLink.forEach((link) => {
+  link.addEventListener('click', toggleMenuDisplay);
+});
