690b9d30ef05a8651b06961b5dbdd14b8e01e79a
Matee <matiullahsafibbc@gmail.com>
2023-02-03 15:10:44 -0600

    debug popup

--- script.js
+++ script.js
@@ -21,52 +21,7 @@ menuLink.forEach((link) => {
   link.addEventListener('click', toggleMenuDisplay);
 });
 
-// Popup
 
-const popupContent = `    <div class="pop-background">
-<div class="popup">
-    <div class="poptop"><h3>Multi Post Stories</h3><img class="closeProject" onClick="togglePopup()" src="./assets/Icon.png" alt="cross">
-    </div>
-        <ul>
-            <li>html</li>
-            <li>Bootstrap</li>
-            <li>Ruby on rails</li>
-        </ul>
-    <img src="./assets/Snapshoot Portfolio.png" alt="IOT 10 image">
-    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent</p>
-    <div class="btn-div"><button>See Live<img src="./assets/Icon - Export.png" alt="export"></button><button>See Source<img src="./assets/Icon -GitHub.png" alt="github"></button></div>
-</div>
-<div class="desktopPopup">
-    <div class="poptop-desktop">
-        <h3>Keeping track of thousands of components website</h3>
-        <img class="closeProject" onClick="togglePopup()" src="/assets/Icon - Cancel.png" alt="cross">
-    </div>
-    <ul>
-        <li>html</li>
-        <li>Bootstrap</li>
-        <li>Ruby on rails</li>
-    </ul>
-    <div class="popDiskDiv">
-        <div>
-            <img id="snapshootDesktop" src="./assets/Snapshoot desktop.png" alt="IOT 10 image">
-        </div>
-        <div>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.</p>
-            <div class="deskBtnDiv"><button>See Live<img src="./assets/Icon - Export Desktop.png" alt="export"></button><button>See Source<img src="./assets/Icon -GitHub-Desktop.png" alt="github"></button></div>
-        </div>
-    </div>
-</div>
-</div>`;
-
-const popDiv = document.querySelector('#portfolio');
-
-popDiv.insertAdjacentHTML('afterend', popupContent);
-
-const togglePopup = () => {
-  document.querySelector('.pop-background').classList.toggle('openPopup');
-};
-
-const closeProject = document.querySelectorAll('closeProject');
 
 // Work cards
 
@@ -172,7 +127,7 @@ const projectDataDesktop = [
 
 for (let i = 0; i < projectData.length; i += 1) {
   const card = document.createElement('div');
-  card.classList.add('project-data', 'color-primary');
+  card.classList.add('project-data', 'color-primary', `card=${projectData[i].id}`);
   card.innerHTML = `
   <h2>${projectData[i].title}</h2>
   <p>${projectData[i].description}</p>
@@ -181,7 +136,7 @@ for (let i = 0; i < projectData.length; i += 1) {
       <li>${projectData[i].technologies[1]}</li>
       <li>${projectData[i].technologies[2]}</li>
   </ul>
- <button id="${projectData[i].id}" class="seeProject-btn color-primary" onclick="togglePopup()">See Project</button>`;
+ <button id="${projectData[i].id}" class="seeProject-btn color-primary" data-id="${projectData[i].id}">See Project</button>`;
 
   document.querySelector('.work-grids').appendChild(card);
 }
@@ -197,10 +152,63 @@ for (let i = 0; i < projectDataDesktop.length; i += 1) {
       <li>${projectDataDesktop[i].technologies[1]}</li>
       <li>${projectDataDesktop[i].technologies[2]}</li>
   </ul>
- <button id="${projectDataDesktop[i].id}" class="seeProject-btn color-primary" onclick="togglePopup()">See Project</button>`;
+ <button id="${projectDataDesktop[i].id}" class="seeProject-btn color-primary" data-idDesktop="${projectData[i].id}">See Project</button>`;
 
   document.querySelector('.work-grids-desktop').appendChild(card);
 }
+
+// Popup
+
+const popupContent = `    <div class="pop-background">
+<div class="popup">
+    <div class="poptop"><h3>Multi Post Stories</h3><img class="closeProject" src="./assets/Icon.png" alt="cross">
+    </div>
+        <ul>
+            <li>html</li>
+            <li>Bootstrap</li>
+            <li>Ruby on rails</li>
+        </ul>
+    <img src="./assets/Snapshoot Portfolio.png" alt="IOT 10 image">
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent</p>
+    <div class="btn-div"><button>See Live<img src="./assets/Icon - Export.png" alt="export"></button><button>See Source<img src="./assets/Icon -GitHub.png" alt="github"></button></div>
+</div>
+<div class="desktopPopup">
+    <div class="poptop-desktop">
+        <h3>Keeping track of thousands of components website</h3>
+        <img class="closeProject" src="/assets/Icon - Cancel.png" alt="cross">
+    </div>
+    <ul>
+        <li>html</li>
+        <li>Bootstrap</li>
+        <li>Ruby on rails</li>
+    </ul>
+    <div class="popDiskDiv">
+        <div>
+            <img id="snapshootDesktop" src="./assets/Snapshoot desktop.png" alt="IOT 10 image">
+        </div>
+        <div>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.</p>
+            <div class="deskBtnDiv"><button>See Live<img src="./assets/Icon - Export Desktop.png" alt="export"></button><button>See Source<img src="./assets/Icon -GitHub-Desktop.png" alt="github"></button></div>
+        </div>
+    </div>
+</div>
+</div>`;
+
+// function
+
+const popDiv = document.querySelector('#portfolio');
+
+popDiv.insertAdjacentHTML('afterend', popupContent);
+
+const togglePopup = () => {
+  document.querySelector('.pop-background').classList.toggle('openPopup');
+};
+
+const projectButtons = document.querySelectorAll('.seeProject-btn');
+projectButtons.forEach((element) => {
+  element.addEventListener('click', togglePopup)
+})
+const closeProject = document.querySelectorAll('.closeProject');
 closeProject.forEach((closePro) => {
   closePro.addEventListener('click', togglePopup);
 });
